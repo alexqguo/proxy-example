c0d150bed8dba53e232777b78d7ae11c4c89e6a3
alexqguo <alexqguo@gmail.com>
2021-07-22 13:16:35 -0400

    Add shell sort

--- index.js
+++ index.js
@@ -122,6 +122,25 @@
     }
     return arr;
   };
+
+  const shellSort = arr => {
+    let n = arr.length;
+
+    for (let gap = Math.floor(n / 2); gap > 0; gap = Math.floor(gap / 2)) {
+      for (let i = gap; i < n; i += 1) {
+        let temp = arr[i];
+
+        let j;
+        for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+          arr[j] = arr[j - gap];
+        }
+
+        arr[j] = temp;
+      }
+    }
+
+    return arr;
+  };
   ////////////////////////////////////////////////////////////
   ////////////////////////////////////////////////////////////
 
@@ -219,6 +238,8 @@
         mergeSort(vizData.proxy);
       case 'quick':
         quickSort(vizData.proxy);
+      case 'shell':
+        shellSort(vizData.proxy);
       case 'native':
         vizData.proxy.sort((a, b) => a - b);
       default:
